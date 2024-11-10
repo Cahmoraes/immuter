f34290357cc4d99c1596d346c7063b963323439c
Caique Moraes <ike.moraes93@gmail.com>
2024-11-10 10:08:31 -0300

    refactor: update build configuration and output formats

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  clean: true,
-  minify: true,
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  treeshake: true,
+  format: ['esm', 'cjs'], // Gera os formatos ESM e CJS
+  splitting: false, // Desativa o splitting para facilitar a compatibilidade
+  sourcemap: false, // Mantém o sourcemap para debugging
+  clean: true, // Limpa a pasta dist antes de compilar
+  minify: true, // Minifica o código
+  dts: true, // Gera arquivos de declaração de tipos (TypeScript)
+  outDir: 'dist', // Define o diretório de saída como 'dist'
 })
